e714ec386129c0503c7a0bf37c625309077e96f2
Shane Powell <shaneapowell@hotmail.com>
2024-06-01 14:05:34 -0500

    Fixing verify action

--- tools/validate_dist.sh
+++ tools/validate_dist.sh
@@ -1,7 +1,12 @@
-HASH1=$(find dist -type f | sort | sha1sum)
+set -eu -o pipefail
+HASH1=$(find dist -type f | sort | sha1sum -b)
 pipenv run compile
-HASH2=$(find dist -type f | sort | sha1sum)
-if [[ "$HASH1" != "$HASH2" ]]; then
+HASH2=$(find dist -type f | sort | sha1sum -b)
+echo "Hash1 [$HASH1]"
+echo "Hash2 [$HASH2]"
+if [ "$HASH1" != "$HASH2" ]; then
 echo "Re-Build of dist folder produced different hash. Did you forget to pre-build?"
 exit 1
+else
+echo "Hash Values match.  dist looks good"
 fi
