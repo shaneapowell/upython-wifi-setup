cffb579fcca2b70d2d7a9b8467e4af232cf53ed8
Shane Powell <shaneapowell@hotmail.com>
2024-06-01 13:54:56 -0500

    Was broken, pushed to see if it's fixed

--- tools/validate_dist.sh
+++ tools/validate_dist.sh
@@ -1,5 +1,5 @@
 HASH1=$(find dist -type f | sort | sha1sum)
-pipenv run build
+pipenv run compile
 HASH2=$(find dist -type f | sort | sha1sum)
 if [[ "$HASH1" != "$HASH2" ]]; then
 echo "Re-Build of dist folder produced different hash. Did you forget to pre-build?"
