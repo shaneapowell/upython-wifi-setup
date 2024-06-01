3b2aa1e25a94803b4bae8a72ee061759a1d8bc0a
Shane Powell <shaneapowell@hotmail.com>
2024-06-01 14:29:29 -0500

    Hash script should work correctly now

--- tools/validate_dist.sh
+++ tools/validate_dist.sh
@@ -1,12 +1,22 @@
 set -eu -o pipefail
-HASH1=$(find dist -type f | sort | sha1sum -b)
+
+# Before
+HASH1=$(find dist -type f | sort | xargs sha256sum)
+
+pipenv run clean
 pipenv run compile
-HASH2=$(find dist -type f | sort | sha1sum -b)
-echo "Hash1 [$HASH1]"
-echo "Hash2 [$HASH2]"
+
+# After
+HASH2=$(find dist -type f | sort | xargs sha256sum)
+
+
+echo -e "HASH1\n$HASH1"
+echo -e "HASH2\n$HASH2"
+
 if [ "$HASH1" != "$HASH2" ]; then
 echo "Re-Build of dist folder produced different hash. Did you forget to pre-build?"
 exit 1
-else
-echo "Hash Values match.  dist looks good"
 fi
+
+
+echo "Hash Values match.  dist looks good"
